cd63d2c2597b4be52d7203ebd491f66debca8de3
Ayesha Firdose W <111958106+ayeshafirdose31@users.noreply.github.com>
2025-05-01 20:56:36 +0530

    Add files via upload

---
+++ WAP7.py
@@ -0,0 +1,8 @@
+#WAP TO CHECK IF A NUMBER IS MULTIPLE OF 7 OR NOT
+
+num=int(input("Enter the number: "))
+
+if num%7==0:
+    print("It is a multiple of 7")
+else:
+    print("It is not a multiple of 7")
