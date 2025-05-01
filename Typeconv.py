e8fc7e53518a5ed6aaffb8f10f00be6ee677f417
Ayesha Firdose W <111958106+ayeshafirdose31@users.noreply.github.com>
2025-05-01 20:55:46 +0530

    Add files via upload

---
+++ Typeconv.py
@@ -0,0 +1,22 @@
+#Type Conversion
+#Implicit, here b has superior vlue and more info so, a gets converted as float and print the value 
+
+a=4
+b=8.23
+sum=a+b
+
+print(sum)
+
+
+#Type Casting 
+#explicitly conversion 
+a=int("2")
+b=34.6
+sum=a+b
+print(sum)
+
+a="Ayesha"
+b="Faisal"
+c="Khan"
+sum=a+" "+b +" "+c
+print(sum)
